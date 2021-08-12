c07b1dea9a0065b0f4d44632277b03ba0af25d76
uneec96 <86100199+uneec96@users.noreply.github.com>
2021-08-12 17:40:23 +0530

    Update instances.tf

--- instances.tf
+++ instances.tf
@@ -5,26 +5,11 @@ resource "aws_instance" "Task" {
 
   tags = {
       Name  = "Task ${count.index}"
+      
     }
 }
 
 resource "aws_ebs_volume" "example" {
   availability_zone = "ap-south-1a"
   size              = 8
-}
-
-resource "aws_volume_attachment" "ebs_att1" {
-  device_name = "/dev/sdh"
-  volume_id   = "${aws_ebs_volume.example.id}"
-  instance_id = "${aws_instance.Task[0].id}"
-}
-resource "aws_volume_attachment" "ebs_att2" {
-  device_name = "/dev/sdh"
-  volume_id   = "${aws_ebs_volume.example.id}"
-  instance_id = "${aws_instance.Task[1].id}"
-}
-resource "aws_volume_attachment" "ebs_att3" {
-  device_name = "/dev/sdh"
-  volume_id   = "${aws_ebs_volume.example.id}"
-  instance_id = "${aws_instance.Task[2].id}"
 }
